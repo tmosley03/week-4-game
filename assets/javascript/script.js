8cf5b3ca1fa20a85c013dbc310ddbe9897e284f2
tmosley03 <tlm3dev@gmail.com>
2018-02-27 13:08:30 -0600

    Completed functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,14 +6,31 @@
 
 // Use this GLOBAL fucntion to generatate all of the numbers.  This particular function will include both the minamum and maximum numbers.//
 
-//=== GLOBAL ===//
+//================================================ GLOBAL ===================================================//
+
 function getRandom(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// create a function that cheks the score against the target number with every click of a stone.
+
+function scoreKeeper() {
+    if (currentScore === targetNumber) {
+        $("#wins").text(wins++);
+        alert("YOU WIN!!!!!");
+        startGame();
+    }
+    else if (currentScore > targetNumber+1) {
+        $("#losses").text(losses++);
+        alert("You have exceeded the target number.  :-( Please try again.");
+        startGame();
+    }
+}
+
 var userScore = 0
+var currentScore = 0
 var wins = 0
 var losses = 0
 
@@ -21,9 +38,9 @@ var stoneOneValue = getRandom(1, 12);
 var stoneTwoValue = getRandom(1, 12);
 var stoneThreeValue = getRandom(1, 12);
 var stoneFourValue = getRandom(1, 12);
-var targetNumber = getRandom(19,120);
-console.log("targetNumber from global "+ targetNumber)
-console.log("stonevalue from global: "+stoneOneValue, stoneTwoValue, stoneThreeValue, stoneFourValue)
+var targetNumber = getRandom(19, 120);
+// console.log("targetNumber from global "+ targetNumber)
+// console.log("stonevalue from global: "+stoneOneValue, stoneTwoValue, stoneThreeValue, stoneFourValue)
 
 //=============== Beginning of Game object litteral =============================//
 crystalsCollectorGame = {
@@ -31,7 +48,11 @@ crystalsCollectorGame = {
 
     //========== Start the Game ===============//
     start: startGame = (function () {
+        currentScore = 0;
+        runningTotal = 0;
         // Set the target number to a random number between 19-120 //
+        targetNumber = getRandom(19,120);
+        console.log("targetNumber from startGame function: " + targetNumber);
         $("#randomNumber").text("Target Number " + targetNumber);
         console.log("targetNumber from startGame function: " + targetNumber);
         // Create a variable for each button and give it a random value between 1-12 //
@@ -71,31 +92,35 @@ $("#startBtn").click(function () {
 //=== Assign numbers to buttons individually.
 // is there a way to check the current value of a variable before executing math?
 $("#stone1").click(function () {
-    currentScore = stoneOneValue.val + userScore.val;
+    currentScore = currentScore + stoneOneValue;
     $("#totalScore").text(currentScore);
-    console.log("currentScore from stone one:  " + currentScore);
-    console.log("stoneValue from button click "+stoneOneValue);
+    scoreKeeper();
+    // console.log("currentScore from stone one:  " + currentScore);
+    // console.log("stoneValue from button click "+stoneOneValue);
 });
 
 $("#stone2").click(function () {
-    currentScore = stoneTwoValue + userScore;
+    currentScore = currentScore + stoneTwoValue;
     $("#totalScore").text(currentScore);
-    console.log("currentScore from stone two:  " + currentScore);
-    console.log("stoneValue from button click "+stoneTwoValue);
+    scoreKeeper();
+    // console.log("currentScore from stone two:  " + currentScore);
+    // console.log("stoneValue from button click "+stoneTwoValue);
 });
 
 $("#stone3").click(function () {
-    currentScore = stoneThreeValue + userScore;
+    currentScore = currentScore + stoneThreeValue;
     $("#totalScore").text(currentScore);
-    console.log("currentScore from stone three:  " + currentScore);
-    console.log("stoneValue from button click "+ stoneThreeValue);
+    scoreKeeper();
+    // console.log("currentScore from stone three:  " + currentScore);
+    // console.log("stoneValue from button click "+ stoneThreeValue);
 });
 
 $("#stone4").click(function () {
-    currentScore = parseInt(stoneFourValue) + parseInt(userScore);
+    currentScore = currentScore + stoneFourValue;
     $("#totalScore").text(currentScore);
-    console.log("currentScore from stone three:  " + currentScore);
-    console.log("stoneValue from button click "+ stoneFourValue);
+    scoreKeeper();
+    // console.log("currentScore from stone three:  " + currentScore);
+    // console.log("stoneValue from button click "+ stoneFourValue);
 });
 
 //========================= Add stone value to user score =============//
@@ -104,10 +129,4 @@ $("#stone4").click(function () {
 
 //========================= Win or Lose Conditions ===================//
 
-if (userScore === targetNumber ){
-    $("#wins").text(wins++);
-}
 
-if(userScore > targetNumber){
-    $("#losses").text(losses++);
-}
